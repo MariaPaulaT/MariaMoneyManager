2830057b7fad2363ca570c341af08d311a748ab0
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-02-08 12:21:33 -0500

    transaction

--- src/main/java/com/app/moneyManager/mariaMoneyManager/entities/Transaction.java
+++ src/main/java/com/app/moneyManager/mariaMoneyManager/entities/Transaction.java
@@ -1,5 +1,58 @@
 package com.app.moneyManager.mariaMoneyManager.entities;
 
-public class Transaction {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
+@Entity
+public class Transaction {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	@Column
+	private double amount;
+	@Column
+	private String type;
+	@Column
+	private String description;
+	public Transaction(int id, double amount, String type, String description) {
+		super();
+		this.id = id;
+		this.amount = amount;
+		this.type = type;
+		this.description = description;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
+	
+
+	
 }
